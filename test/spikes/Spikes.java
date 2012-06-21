8ff29060abc5fb3bebbbc95eb436f196972140ae
Masud Khan <masudio@gmail.com>
2012-06-21 07:16:26 -0600

    figured out how to convert byte[] to hexString.

--- test/spikes/Spikes.java
+++ test/spikes/Spikes.java
@@ -1,8 +1,9 @@
 package spikes;
 
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -15,13 +16,17 @@ public class Spikes
 		try
 		{
 			MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
-			digest.update("aString".getBytes());
+			digest.update("test".getBytes());
 			byte[] bytes = digest.digest();
 			
-			for(int i = 0; i < 4; i++)
-			{
-				Integer value = new Integer(Arrays.copyOfRange(bytes, i * 4, i*5));
-			}
+			System.out.println(new String(bytes));
+			ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+			IntBuffer intBuffer = byteBuffer.asIntBuffer();
+			
+			System.out.println(intBuffer.get());
+			System.out.println(intBuffer.get());
+			System.out.println(intBuffer.get());
+			System.out.println(intBuffer.get());
 		}
 		catch(NoSuchAlgorithmException e)
 		{
@@ -29,4 +34,42 @@ public class Spikes
 		}
 	}
 
+	@Test
+	public void digestToHexString()
+	{
+		try
+		{
+			MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+			digest.update("test".getBytes());
+			byte[] bytes = digest.digest();
+			
+			StringBuilder hexString = new StringBuilder();
+			String hex = "";
+			for(byte b : bytes)
+            {
+				hex = Integer.toHexString(0xFF & b);
+//				if(hex.length() == 1) hexString.append('0');
+	            hexString.append(hex);
+            }
+			
+			System.out.println("hexString: " + hexString.toString());
+			System.out.println(hexString.length());
+		}
+		catch(NoSuchAlgorithmException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void checkEndianness()
+	{
+		byte[] bytes = new byte[]{ 0,1,2,3 };
+		String s = new String(bytes);
+		byte[] result = s.getBytes();
+		for(byte b : result)
+        {
+	        System.out.println(b);
+        }
+	}
 }
--- test/unit/HashFunctionSpecs.java
+++ test/unit/HashFunctionSpecs.java
@@ -20,5 +20,4 @@ public class HashFunctionSpecs
 		
 		assertEquals(expectedResult, result);
 	}
-
 }
